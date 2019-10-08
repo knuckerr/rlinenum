23cf8694489d76ae6e039f4d391367152a731680
Administator <knucker@KnuckerPc.localdomain>
2019-10-08 16:02:22 +0300

    add process list

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,7 @@ mod export;
 mod cmds;
 mod inotify;
 mod procc;
-use inotify::inotify::{init,add_watch,read_events};
-use procc::procclist::getpids;
-use procc::procclist::proc_cmd_read;
-use procc::procclist::proc_status_read;
+use procc::procclist::start;
 
 
 fn main() {
@@ -28,6 +25,7 @@ fn main() {
     }
     // We create a new inotify instance.
     let inotify = init().unwrap();
+    
 
 
     // We add a new watch on directory "test" for all events.
@@ -36,12 +34,7 @@ fn main() {
     let events = inotify.read_events().unwrap();
     read_events(events);
     */
-    let a = getpids();
-    for pid in a.unwrap(){
-        let cmd = proc_cmd_read(pid);
-        let status = proc_status_read(pid);
-        println!("{:?},{:?}",cmd,status);
 
-    }
+    start();
 
 }
--- src/procc/procclist.rs
+++ src/procc/procclist.rs
@@ -3,7 +3,32 @@ use std::fs::File;
 use std::fs::ReadDir;
 use std::io::prelude::*;
 use std::path::Path;
+use std::collections::HashMap;
 
+pub struct Event {
+    uuid: i32,
+    pid: i32,
+    cmd: String,
+}
+
+pub struct Events {
+    cmds: HashMap<i32,Event>
+}
+
+
+pub fn refresh(events:&mut Events) -> Result<&mut Events,Error> {
+    let pids = getpids()?;
+    for pid in pids {
+        if !events.cmds.contains_key(&pid) {
+            let _cmd = proc_cmd_read(pid)?;
+            let _uuid = get_uuid(pid)?;
+            let event = Event{pid:pid,cmd:_cmd,uuid:_uuid};
+            println!("PID: {},UID:{} cmd:{}",event.pid,event.uuid,event.cmd);
+            events.cmds.insert(pid,event);
+        }
+    }
+    Ok(events)
+}
 pub fn read_proc_dir() -> Result<ReadDir, Error> {
     let dir = Path::new("/proc").read_dir()?;
     Ok(dir)
@@ -49,4 +74,36 @@ pub fn getpids() -> Result<Vec<i32>, Error> {
         }
     }
     Ok(pids)
-} 
+}
+
+pub fn get_uuid(pid: i32) -> Result<i32, Error> {
+    let status = proc_status_read(pid)?;
+    let lines: Vec<&str> = status.split("\n").collect();
+    if lines.len() < 9 {
+        err_msg("No uuid found");
+    }
+    let uuid1: Vec<&str> = lines[8].split("\t").collect();
+    if uuid1.len() < 2 {
+        err_msg("No uuid found");
+    }
+    let uuid = uuid1[1].parse::<i32>()?;
+
+    Ok(uuid)
+}
+
+pub fn start() -> Result<(),Error>{
+    let pids = getpids()?;
+    let mut events = HashMap::new();
+    for pid in pids {
+        let _cmd = proc_cmd_read(pid)?;
+        let _uuid = get_uuid(pid)?;
+        let event = Event{pid:pid,cmd:_cmd,uuid:_uuid};
+        println!("PID: {},UID:{} cmd:{}",event.pid,event.uuid,event.cmd);
+        events.insert(pid,event);
+    }
+    let mut all_events = Events{cmds:events}; 
+    loop {
+        refresh(&mut all_events)?;
+    }
+
+}
